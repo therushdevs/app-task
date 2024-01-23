09ec77b8fc60f93f6177c55fecc73e9b5b22fa6f
Rushikesh Kumbhar <mailmerpvk@gmail.com>
2024-01-23 19:05:28 +0530

    base setup

---
+++ lib/mobigic/ui/mobigic_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MobigicHomePage extends StatefulWidget {
+  const MobigicHomePage({super.key});
+
+  @override
+  State<MobigicHomePage> createState() => _MobigicHomePageState();
+}
+
+class _MobigicHomePageState extends State<MobigicHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
