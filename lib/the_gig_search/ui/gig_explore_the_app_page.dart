1bc867f13e5906e94c3f5ae92092e8fabdd7c35b
Rushikesh Kumbhar <mailmerpvk@gmail.com>
2024-02-07 04:49:29 +0530

    create basic ui for explore app apge

--- lib/the_gig_search/ui/gig_explore_the_app_page.dart
+++ lib/the_gig_search/ui/gig_explore_the_app_page.dart
@@ -1,3 +1,4 @@
+import 'package:api_task/core/assets.dart';
 import 'package:flutter/material.dart';
 
 class GigExploreTheAppPage extends StatefulWidget {
@@ -10,6 +11,23 @@ class GigExploreTheAppPage extends StatefulWidget {
 class _GigExploreTheAppPageState extends State<GigExploreTheAppPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.asset(Assets.gigLogo),
+          const Text('Explore the App'),
+          const Text(
+              'Now your finances are in one place and always under control'),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Sign In'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Create Account'),
+          )
+        ],
+      ),
+    );
   }
 }
