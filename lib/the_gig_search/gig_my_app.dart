069932e9c8ac3039cc5378bf00045588bdf33b38
Rushikesh Kumbhar <mailmerpvk@gmail.com>
2024-02-06 18:13:16 +0530

    initial project setup

---
+++ lib/the_gig_search/gig_my_app.dart
@@ -0,0 +1,59 @@
+import 'package:api_task/the_gig_search/gig_splash_page.dart';
+import 'package:flutter/material.dart';
+
+class GigMyApp extends StatelessWidget {
+  const GigMyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // navigatorKey: navKey,
+      // initialRoute: Routes.splashPageRoute,
+      // onGenerateRoute: RouteGenerator.onGenerateRoute,
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 245, 248, 250),
+        useMaterial3: false,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          foregroundColor: Colors.black,
+          elevation: 0,
+          backgroundColor: Color.fromARGB(255, 245, 248, 250),
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        elevatedButtonTheme: const ElevatedButtonThemeData(
+          style: ButtonStyle(
+            alignment: Alignment.center,
+            elevation: MaterialStatePropertyAll(0),
+            // backgroundColor: const MaterialStatePropertyAll(
+            //     Color.fromARGB(255, 143, 157, 247)),
+            // shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            //   RoundedRectangleBorder(
+            //     borderRadius: BorderRadius.circular(6.0),
+            //     side: BorderSide(
+            //       color: Colors.blue.shade700,
+            //       width: 2.5,
+            //       style: BorderStyle.solid,
+            //     ),
+            //   ),
+            // ),
+          ),
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          type: BottomNavigationBarType.fixed,
+          selectedIconTheme: IconThemeData(
+            color: Colors.blue,
+          ),
+          unselectedIconTheme: IconThemeData(
+            color: Colors.grey,
+          ),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const GigSplashPage(),
+    );
+  }
+}
---
+++ lib/the_gig_search/gig_splash_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class GigSplashPage extends StatefulWidget {
+  const GigSplashPage({super.key});
+
+  @override
+  State<GigSplashPage> createState() => _GigSplashPageState();
+}
+
+class _GigSplashPageState extends State<GigSplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Splash Page'),
+      ),
+    );
+  }
+}
