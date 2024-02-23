04855d9ebc2f0498f5a07e21c9c255325902bf2f
Rushikesh Kumbhar <mailmerpvk@gmail.com>
2024-02-23 14:01:11 +0530

    add pop condition

--- lib/cognota/ui/home_page_cognota.dart
+++ lib/cognota/ui/home_page_cognota.dart
@@ -15,55 +15,60 @@ class HomePageCognota extends StatefulWidget {
 class _HomePageCognotaState extends State<HomePageCognota> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tasks'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {
-          cNavKey.currentState!.pushNamed(Routes.createTaskPageRoute);
-        },
-      ),
-      body: SafeArea(
-        child: FutureBuilder(
-          future: _tasksFuture(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              final List<TaskModel>? data = snapshot.data;
-
-              return data != null && data.isNotEmpty
-                  ? ListView.builder(
-                      itemCount: data.length,
-                      itemBuilder: (context, index) {
-                        return Card(
-                          child: ListTile(
-                            title: Text('Title: ${data[index].title}'),
-                            subtitle: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Description: ${data[index].description}'),
-                                Text('Date & Time: ${data[index].dateTime}'),
-                              ],
-                            ),
-                          ),
-                        );
-                      },
-                    )
-                  : const Center(
-                      child: Text('No Tasks present'),
-                    );
-            } else if (snapshot.hasError) {
-              print('Error: ${snapshot.error}');
-              return const Center(
-                child: Text('Something went wrong'),
-              );
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: const Text('Tasks'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            cNavKey.currentState!.pushNamed(Routes.createTaskPageRoute);
           },
+        ),
+        body: SafeArea(
+          child: FutureBuilder(
+            future: _tasksFuture(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final List<TaskModel>? data = snapshot.data;
+
+                return data != null && data.isNotEmpty
+                    ? ListView.builder(
+                        itemCount: data.length,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            child: ListTile(
+                              title: Text('Title: ${data[index].title}'),
+                              subtitle: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                      'Description: ${data[index].description}'),
+                                  Text('Date & Time: ${data[index].dateTime}'),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      )
+                    : const Center(
+                        child: Text('No Tasks present'),
+                      );
+              } else if (snapshot.hasError) {
+                print('Error: ${snapshot.error}');
+                return const Center(
+                  child: Text('Something went wrong'),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
         ),
       ),
     );
